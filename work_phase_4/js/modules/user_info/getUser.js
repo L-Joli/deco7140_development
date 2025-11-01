d3f372cddce1262319254e68c0a19921cbf232fa
Joli <69351113+L-Joli@users.noreply.github.com>
2025-11-01 21:01:21 +1000

    add getUser api

---
+++ work_phase_4/js/modules/user_info/getUser.js
@@ -0,0 +1,17 @@
+import { fetchData } from "../fetchData.js";
+
+export const getUser = async () => {
+    try {
+        const data = await fetchData("community");
+
+        if (!data) {
+            throw new Error("No data returned from server.");
+        }
+
+        return data[0];
+
+    } catch (error) {
+        console.error("Error in getRepos:", error);
+        return null;
+    }
+};
